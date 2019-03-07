4002416e1a868bba110536261f61ded78840b82b
LEONARDO BATISTA RODRIGUES FREUA <ALU201514123@uniritter.local>
2019-03-07 20:04:40 -0300

    Created the Facade pattern.

---
+++ Facade.py
@@ -0,0 +1,109 @@
+#!/usr/bin/python
+# -*- coding : utf-8 -*-
+
+
+"""
+The Facade design pattern is one of the twenty-three well-known GoF design patterns that describe
+how to solve recurring design problems to design flexible and reusable object-oriented software,
+that is, objects that are easier to implement, change, test, and reuse.
+
+*What problems can the Facade design pattern solve?
+
+- To make a complex subsystem easier to use, a simple interface should be provided for a set of
+interfaces in the subsystem.
+- The dependencies on a subsystem should be minimized.
+
+Clients that access a complex subsystem directly refer to (depend on) many different objects having
+different interfaces (tight coupling), which makes the clients hard to implement, change, test, and reuse.
+
+*What solution does the Facade design pattern describe?
+
+Define a Facade object that
+
+- implements a simple interface in terms of (by delegating to) the interfaces in the subsystem and
+- may perform additional functionality before/after forwarding a request.
+"""
+
+
+from __future__ import print_function
+import time
+
+SLEEP = 0.1
+
+
+# Complex Parts
+class TC1:
+    def run(self):
+        print(u"###### In Test 1 ######")
+        time.sleep(SLEEP)
+        print(u"Setting up")
+        time.sleep(SLEEP)
+        print(u"Running test")
+        time.sleep(SLEEP)
+        print(u"Tearing down")
+        time.sleep(SLEEP)
+        print(u"Test Finished\n")
+
+
+class TC2:
+    def run(self):
+        print(u"###### In Test 2 ######")
+        time.sleep(SLEEP)
+        print(u"Setting up")
+        time.sleep(SLEEP)
+        print(u"Running test")
+        time.sleep(SLEEP)
+        print(u"Tearing down")
+        time.sleep(SLEEP)
+        print(u"Test Finished\n")
+
+
+class TC3:
+    def run(self):
+        print(u"###### In Test 3 ######")
+        time.sleep(SLEEP)
+        print(u"Setting up")
+        time.sleep(SLEEP)
+        print(u"Running test")
+        time.sleep(SLEEP)
+        print(u"Tearing down")
+        time.sleep(SLEEP)
+        print(u"Test Finished\n")
+
+
+# Facade
+class TestRunner:
+    def __init__(self):
+        self.tc1 = TC1()
+        self.tc2 = TC2()
+        self.tc3 = TC3()
+        self.tests = [self.tc1, self.tc2, self.tc3]
+
+    def runAll(self):
+        [i.run() for i in self.tests]
+
+
+# Client
+if __name__ == '__main__':
+    testrunner = TestRunner()
+    testrunner.runAll()
+
+### OUTPUT ###
+# ###### In Test 1 ######
+# Setting up
+# Running test
+# Tearing down
+# Test Finished
+#
+# ###### In Test 2 ######
+# Setting up
+# Running test
+# Tearing down
+# Test Finished
+#
+# ###### In Test 3 ######
+# Setting up
+# Running test
+# Tearing down
+# Test Finished
+#
