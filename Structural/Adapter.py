73238ffe3f681b38e6ef2766b3cd7cb04ceeb1d0
Leonardo Freua <leonardo.freua@ntconsult.com.br>
2019-03-08 11:02:27 -0300

    Created the Adapter pattern

--- Structural/Adapter.py
+++ Structural/Adapter.py
@@ -18,7 +18,7 @@ conform to the interface clients require.
 
 *The adapter design pattern describes how to solve such problems:
 
-- Define a separate adapter class that converts the (incompatible) interface of a class (adaptee)
+- Define a separate adapter class that converts the (incompatible) interface of a class (adapter)
 into another interface (target) clients require.
 - Work through an adapter to work with (reuse) classes that do not have the required interface.
 
@@ -69,5 +69,55 @@ class USASocketInterface:
     def neutral(self):
         pass
 
-#Adapter
+
+# Adapter
 class Adapter(USASocketInterface):
+    __socket = None
+
+    def __init__(self, socket):
+        self.__socket = socket
+
+    def voltage(self):
+        return 110
+
+    def live(self):
+        return self.__socket.live()
+
+    def neutral(self):
+        return self.__socket.neutral()
+
+
+# Client
+class EletrictKettle:
+    __power = None
+
+    def __init__(self, power):
+        self.__power = power
+
+    def boil(self):
+        if self.__power.voltage() > 110:
+            print("Kettle on fire!")
+        else:
+            if self.__power.live() == 1 and self.__power.neutral() == -1:
+                print("Coffee Time!")
+            else:
+                print("No power")
+
+
+def main():
+    socket = Socket()
+    adapter = Adapter(socket)
+    kettle = EletrictKettle(adapter)
+
+    # Make Coffee
+    kettle.boil()
+
+    return 0
+
+
+if __name__ == '__main__':
+    main()
+
+# ---- OUTPUT: ---- :
+#
+# Coffee Time!
