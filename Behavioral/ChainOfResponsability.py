1451316d68d0a56b13eed5daa2b4f92ffe978927
leonardo freua <leonardo.batista.freua@gmail.com>
2019-03-12 17:24:21 -0300

    adjusted formatting.

--- Behavioral/ChainOfResponsability.py
+++ Behavioral/ChainOfResponsability.py
@@ -11,11 +11,14 @@ reusable object-oriented software, that is, objects that are easier to implement
 
 - Coupling the sender of a request to its receiver should be avoided.
 - It should be possible that more than one receiver can handle a request.
-  Implementing a request directly within the class that sends the request is inflexible because it couples the class to a particular receiver and makes it impossible to support multiple receivers.
+
+Implementing a request directly within the class that sends the request is inflexible because
+it couples the class to a particular receiver and makes it impossible to support multiple receivers.
 
 *What solution does the Chain of Responsibility design pattern describe?
 
-- Define a chain of receiver objects having the responsibility, depending on run-time conditions, to either handle a request or forward it to the next receiver on the chain (if any).
+- Define a chain of receiver objects having the responsibility, depending on run-time conditions, t
+  o either handle a request or forward it to the next receiver on the chain (if any).
 
 Rereference: https://en.wikipedia.org/wiki/Chain-of-responsibility_pattern
 """
@@ -78,7 +81,6 @@ class TextHandler(Handler):
 
         for line in text:
             print(line)
-
 
 
 class ErrorHandler(Handler):
