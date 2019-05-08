990b44870930085996888a3d34b6b5944494f0c1
Leonardo Freua <leonardo.batista.freua@gmail.com>
2019-05-07 21:50:41 -0300

    created the Visitor pattern.

--- Behavioral/Visitor.py
+++ Behavioral/Visitor.py
@@ -26,5 +26,74 @@ software, that is, objects that are easierto implement, change, test, and reuse.
 Reference: https://en.wikipedia.org/wiki/Visitor_pattern
 """
 
+class Expr(object):
+
+    def accept(self, visitor):
+        method_name = 'visit_{}'.format(self.__class__.__name__.lower())
+        visit = getattr(visitor, method_name)
+        return visit(self)
+
+
+class Int(Expr):
+
+    def __init__(self, value):
+        self.value = value
+
+
+class Add(Expr):
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+
+class Mul(Expr):
+
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+        
+class Visitor(object):
+    pass
+
+
+class Eval(Visitor):
+
+    def visit_init(self, i):
+        return i.value
+
+    def visit_add(self, a):
+        return a.left.accept(self) + a.right.accept(self)
+
+    def visit_mul(self, a):
+        return a.left.accept(self) * a.right.accept(self)
+
+
+class Print(Visitor):
+
+    def visit_init(self, i):
+        return i.value
+
+    def visit_add(self, a):
+        return '{+{} {}}'.format(a.left.accept(self), a.right.accept(self))
+
+    def visit_mul(self, a):
+        return '{+{} {}}'.format(a.left.accept(self), a.right.accept(self))
+
+
+def main():
+    expr = Add(
+            Add(Int(4), Int(3)), 
+            Mul(Int(10), Add(Int(1), Int(1)))
+        )
+    print(expr.accept(Print()))
+    print(expr.accept(Eval()))
+
+
+if __name__ == "__main__":
+    main()
+
+    
 # ---- OUTPUT: ---- :
 #
